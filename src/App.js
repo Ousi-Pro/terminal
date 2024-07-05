5654026df9d9d20910243089160d189031de159a
Oussama Daraâoui <116225179+Ousi-Pro@users.noreply.github.com>
2024-07-06 00:24:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {React, Component} from 'react'
-import './src/App.css';
+import 'terminal/src/App.css';
 import RegCompo from './RegCompo.js';
 export class App extends Component {
   render() {
